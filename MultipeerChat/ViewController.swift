00970ea7367b62fcb29f3833e5fbd5f6fba6a709
akmalh <shuvo707@hotmail.com>
2016-01-20 12:26:32 +1100

    Initial Commit

---
+++ MultipeerChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultipeerChat
+//
+//  Created by Akmal Hossain on 20/01/2016.
+//  Copyright (c) 2016 Akmal Hossain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
